347ac155a3dce952ec649aa4d0211157b173e75c
Mridul Choudhary <mridul.choudhary7@gmail.com>
2024-11-11 09:27:37 +0530

    Init commit

---
+++ src/main/java/io/github/mycodethesaurus/notifier/controller/PingController.java
@@ -0,0 +1,34 @@
+package io.github.mycodethesaurus.notifier.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@RestController
+@RequestMapping("/v1")
+public class PingController {
+  public InetAddress ip;
+  public String hostname;
+
+  @GetMapping("/ping")
+  public String ping() {
+    return "Hello from AWS EC2";
+  }
+
+  @GetMapping("/host")
+  public String getHostDetails() {
+
+    try {
+      ip = InetAddress.getLocalHost();
+      hostname = ip.getHostName();
+      System.out.println("Your current IP address : " + ip);
+      System.out.println("Your current Hostname : " + hostname);
+    } catch (UnknownHostException e) {
+      return "Exception occurred while processing the request: " + e.getMessage();
+    }
+    return ip + ":" + hostname;
+  }
+}
